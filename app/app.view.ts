a25add48309851d1dd66ff17189295ca1c84e773
Lyumih <m.ivanov1225@gmail.com>
2024-05-10 00:06:43 +0300

    добавил логин

--- app/app.view.ts
+++ app/app.view.ts
@@ -12,6 +12,10 @@ namespace $.$$ {
 			return $mol_state_local.value<User>( 'user', next === null ? null : $sib_fetch.post<User, UserProps>( '/auth', next ) )
 		}
 
+		hero_name(): string {
+			return $sib_app.user() ? `${$sib_app.user()?.name}@${$sib_app.user()?.login}` : '-'
+		}
+
 		body(): readonly any[] {
 			return [ $sib_app.user() ? this.Pages() : this.Auth() ]
 		}
