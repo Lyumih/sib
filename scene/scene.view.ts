8a235d284c3fbedb26608a4eeeafd7af7b59d745
Lyumih <m.ivanov1225@gmail.com>
2024-05-10 03:43:18 +0300

    add 2 scene

--- scene/scene.view.ts
+++ scene/scene.view.ts
@@ -7,13 +7,18 @@ namespace $.$$ {
 
 	export class $sib_scene extends $.$sib_scene {
 		
+		@$mol_mem
 		island() {
-			console.log('sib scene', this.island_id())
 			return $sib_island.islands()?.find( island => island.id === this.island_id() )
 		}
 
 		scenes() {
 			return this.island()?.scenes || []
+		}
+
+		step(): string {
+			console.log($mol_state_arg.value('step'))
+			return $mol_state_arg.value( 'step' ) || '0'
 		}
 
 		@$mol_mem
@@ -28,15 +33,14 @@ namespace $.$$ {
 		@$mol_mem
 		question(): string {
 			console.log( this.current_scene(), this.scenes(), this.island_id(), this.island(), $sib_island.islands() )
-			return this.current_scene()?.question || 'Задайте вопрос'
+			return this.current_scene()?.question || '! Неизвестное место'
 		}
 
 		@$mol_mem
 		bg_url() {
 			// https://wallpaperscraft.ru/wallpaper/nebo_oblaka_otrazheniia_86205 - Сайт с картинками
 			const base_background = 'https://images.wallpaperscraft.ru/image/single/nebo_oblaka_otrazheniia_86205_1280x720.jpg'
-			// return `center / cover no-repeat url(${ this.current_scene()?.bg || base_background })`
-			return `center / cover no-repeat url(${ base_background })`
+			return `center / cover no-repeat url(${ this.current_scene()?.bg || base_background })`
 		}
 		
 	}
